5297fc0faf1ab4788b0c13234eb0527261110fdf
Gillys Dantas <gillys.ayres@gmail.com>
2021-04-13 22:11:51 -0300

    Added new validation to Review

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
-  validates :content, presence: true
+  validates :content, :rating, presence: true
   validates :rating, { in: %w(0..5), message: "%{value} is not a valid rating" }
 end
