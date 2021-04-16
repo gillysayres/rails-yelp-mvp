c8b988ef6f16a93250ae408a39f573150c9ba8ae
Gillys Dantas <gillys.ayres@gmail.com>
2021-04-16 11:11:39 -0300

    Added private method reviewscontroller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,9 +1,16 @@
 class ReviewsController < ApplicationController
+  before_action :find_restaurant
   def new
     @review = Review.new
   end
 
   def create
 
+  end
+
+  private
+
+  def find_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
   end
 end
