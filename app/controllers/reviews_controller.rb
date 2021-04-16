23c0270b343fb6f1ed30ffe74e670d38a854886a
Gillys Dantas <gillys.ayres@gmail.com>
2021-04-16 11:28:54 -0300

    Added coded create action in reviewscontroller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,12 +5,19 @@ class ReviewsController < ApplicationController
   end
 
   def create
-
+    @review = Review.net(review_params)
+    @review.restaurant = @restaraunt
+    @review.save
+    redirect_to restaraurant_path(@restaurant)
   end
 
   private
 
   def find_restaurant
     @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:rating, :content)
   end
 end
